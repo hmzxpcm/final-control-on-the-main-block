ffd34d30cfcba9e4e7e7b9b359dca6273546879c
hmzxp <hmzxp13@gmail.com>
2023-08-06 17:36:40 +0300

    feat: добавил реализацию программы

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿string[] arr = new string[] { "123", "1234", "234", "2356" };
+int maxLength = 3;
+
+void PrintStringArray(string[] stringsArray)
+{
+    for (int i = 0; i < stringsArray.Length; i++)
+    {
+        Console.Write(stringsArray[i]);
+
+        if (i + 1 != stringsArray.Length) Console.Write(", ");
+    }
+}
+
+string[] GetStringsByMaxLength(string[] stringsArray, int maxLength)
+{
+    return Array.FindAll(stringsArray, (str) => str.Length <= maxLength);
+}
+
+PrintStringArray(
+GetStringsByMaxLength(arr, maxLength)
+);
